9f0dc49e7e599a6afaff6123b94ff162df9b337d
Evgenia Milcheva <evgenia.milcheva@hotmail.com>
2021-12-15 17:32:02 +0200

    add dynamic images for blog posts

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,21 +2,30 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle="Super Cool Blog Posts">
-      <p>My blog post contents will go here (eventually).</p>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
 };
 export const query = graphql`
   query ($id: String) {
-    mdx(id: { eq: $id }) {
+    mdx(id: { eq: $id }, frontmatter: {}) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
